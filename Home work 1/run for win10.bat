a83c5afbb827a4a0a6d481322888b6ed6739a063
ALEX <alexsmi0807@mail.ru>
2022-07-08 22:01:46 +0300

    change name libs to requirements

--- Home work 1/run for win10.bat
+++ Home work 1/run for win10.bat
@@ -1,3 +1,3 @@
-py -m pip install -r libs.txt
+py -m pip install -r requirements.txt
 py -m jupyter nbconvert --execute .\HomeWork1.ipynb --to html
 start HomeWork1.html
--- Home work 1/run.bat
+++ Home work 1/run.bat
@@ -1,2 +1,2 @@
-pip install -r libs.txt
+pip install -r requirements.txt
 jupyter nbconvert --execute .\HomeWork1.ipynb --to html
