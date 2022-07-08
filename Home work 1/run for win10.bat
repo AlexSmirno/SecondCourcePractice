d3d55b9b04abaeba44603d604688660ddc51e82b
ALEX <alexsmi0807@mail.ru>
2022-07-08 14:24:42 +0300

    All to main
    All to main

---
+++ Home work 1/run for win10.bat
@@ -0,0 +1,3 @@
+py -m pip install -r libs.txt
+py -m jupyter nbconvert --execute .\HomeWork1.ipynb --to html
+start HomeWork1.html
---
+++ Home work 1/run.bat
@@ -0,0 +1,2 @@
+pip install -r libs.txt
+jupyter nbconvert --execute .\HomeWork1.ipynb --to html
